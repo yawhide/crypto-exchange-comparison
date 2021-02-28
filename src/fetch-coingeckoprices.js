d60f48ed20031944d9d51d57b26f27f3350f99bf
mcscrad <49381384+mcscrad@users.noreply.github.com>
2021-02-27 20:07:12 -0500

    query price-poller instead of hosting coingecko prices

--- src/fetch-coingeckoprices.js
+++ src/fetch-coingeckoprices.js
@@ -1,7 +1,9 @@
 import useSWR from "swr";
 
 export default function CoingeckoPriceData() {
-  const { data, error } = useSWR(`/api/coingecko-prices`);
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_PRICES_HOST}/prices`
+  );
 
   return {
     data: data,
